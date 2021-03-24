12d3a22bba47a295ed4deac339fcd1cfabdbc439
Pedro Paulo Simionato <pedro.psimionato@gmail.com>
2021-03-23 22:14:46 -0300

    Projeto iniciado, simula uma conta bancaria

---
+++ ContaBancaria/Conta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    class Conta
+    {
+        public string Nome { get, private set };
+        public int DadosConta { get, private set };
+        public double Saldo { get, private set };
+
+        public Conta(double saldo)
+        {
+            Saldo = saldo;
+        }
+
+        public Conta(string nome, int dadosconta, double saldo) // Construtor para leitura do nome e numero da conta
+        {
+            Nome = nome;
+            DadosConta = dadosconta;
+            Saldo = saldo;
+        }
+
+        public void Saque(double saque)
+        {
+            Saldo = Saldo - saque - 5.0;
+        }
+
+        public void Deposito(double deposito)
+        {
+            Saldo = Saldo + deposito;
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine("Dados da conta atualizados: ");
+            return "Conta " + DadosConta + ", Titutlar: " + Nome + ", Saldo: $ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta c;
+            double saldo = 0;
+
+            Console.Write("Entre o número da conta: ");
+            int dadosConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+
+            
+            Console.Write("Haverá depósito inicial(s / n) ? ");
+            char depositoInicial = char.Parse(Console.ReadLine());
+            if (depositoInicial == 's')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                
+            }
+            Console.WriteLine();
+
+            c = new Conta(nome, dadosConta, saldo); // Leitura dos dados iniciais e atribuição ao construtor.
+            Console.WriteLine(c);
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para depósito: ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Deposito(valor);
+            Console.WriteLine(c);
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para saque: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Saque(valor);
+            Console.WriteLine(c);
+            Console.WriteLine();
+        }
+    }
+}
